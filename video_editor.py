9442403f94674228606c6b3ca716f2ff7df2d16f
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-08-18 23:52:56 +0200

    Worked on shortening the subtitle segments

--- video_editor.py
+++ video_editor.py
@@ -1,8 +1,15 @@
 import time
 import math
-import ffmpeg
-from moviepy.editor import VideoFileClip, AudioFileClip
+# import ffmpeg
+from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip
+from moviepy.video.tools.subtitles import SubtitlesClip
 from faster_whisper import WhisperModel
+from moviepy.config import change_settings
+
+SUBTITLE_FILE_NAME = "subtitles.srt"
+
+# Change this to the path of your ImageMagick installation (autodetection doesn't work)
+change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
 
 def combine_sound_and_video(video_path, audio_path, output_path):
   video = VideoFileClip(video_path)
@@ -15,17 +22,19 @@ def add_subtitles(input_path, audio_path, output_path):
   # Followed this guide: https://www.digitalocean.com/community/tutorials/how-to-generate-and-add-subtitles-to-videos-using-python-openai-whisper-and-ffmpeg
   # Used this to get faster whisper working: https://stackoverflow.com/questions/66355477/could-not-load-library-cudnn-ops-infer64-8-dll-error-code-126-please-make-sure
   
-  language, segments = transcribe(audio_path)
-  subtitle_file = generate_subtitle_file(language, segments)
-  render_subtitle_to_video(subtitle_file, input_path, output_path)
+  segments = transcribe(audio_path)
+  print("Transcription - complete")
+  generate_subtitle_file(segments)
+  print("Subtitle file - created")
+
+#   render_subtitle_to_video(input_path, output_path)
 
 
 def transcribe(audio):
     model = WhisperModel("small", device="cuda", compute_type="float32")
-    segments, info = model.transcribe(audio, beam_size=5)
-    language = info[0]
+    segments, _ = model.transcribe(audio, language='en')
     segments = list(segments)
-    return language, segments
+    return segments
 
 def format_time(seconds):
     hours = math.floor(seconds / 3600)
@@ -38,9 +47,8 @@ def format_time(seconds):
 
     return formatted_time
 
-def generate_subtitle_file(language, segments, input_video_name="video"):
+def generate_subtitle_file(segments, input_video_name="video"):
 
-    subtitle_file = f"sub-{input_video_name}.{language}.srt"
     text = ""
     for index, segment in enumerate(segments):
         segment_start = format_time(segment.start)
@@ -49,35 +57,27 @@ def generate_subtitle_file(language, segments, input_video_name="video"):
         text += f"{segment_start} --> {segment_end} \n"
         text += f"{segment.text} \n"
         text += "\n"
-        
-    f = open(subtitle_file, "w")
-    f.write(text)
-    f.close()
-
-    STYLE = """[Script Info]
-Title: Default Aegisub file
-ScriptType: v4.00+
-
-[V4+ Styles]
-Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Alignment
-
-Style: Default,Britannic Bold,24,&H000000,&H00FFFF,&H00000000,&H99000000,-1,0,6
-
-"""
 
     # Save subtitle file
-    subtitle_file_path = f"./{subtitle_file}"
-    f = open(subtitle_file_path, "w")
-    f.write(text)
-    f.close()
+    with open(SUBTITLE_FILE_NAME, "w") as f:
+        f.write(text)
 
-    return subtitle_file
-
-def render_subtitle_to_video(subtitle_file, input_video, output_video):
+def render_subtitle_to_video(input_path, output_path):
     # https://superuser.com/questions/874598/creating-video-containing-animated-text-using-ffmpeg-alone/874697#874697
-    #     
-    video_input_stream = ffmpeg.input(input_video)
-    stream = ffmpeg.output(video_input_stream, output_video, vf=f"subtitles={subtitle_file}")
-    ffmpeg.run(stream, overwrite_output=True, cmd=r'c:\FFmpeg\bin\ffmpeg.exe')
+
+    generator = lambda txt: TextClip(txt, font='Georgia-Regular', fontsize=24, color='white')
+    video_clip = VideoFileClip(input_path)
+    subtitles_clip = SubtitlesClip(SUBTITLE_FILE_NAME, generator)
+    # for (_, txt) in subtitles_clip:
+    #     print(txt)
+    print(subtitles_clip.duration)
+    # text_clips = [TextClip(txt, fontsize=24, color='white').set_position('bottom') for (_, txt) in subtitles_clip]
+    final_clip = CompositeVideoClip([video_clip, subtitles_clip])
+    final_clip.duration = video_clip.duration
+    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
+
+    # video_input_stream = ffmpeg.input(input_video)
+    # stream = ffmpeg.output(video_input_stream, output_path, vf=f"subtitles={subtitle_file}")
+    # ffmpeg.run(stream, overwrite_output=True, cmd=r'c:\FFmpeg\bin\ffmpeg.exe')
 
 add_subtitles('./output/clean/tiktok.mp4', './speech.mp3', './output/final/tiktok.mp4')
