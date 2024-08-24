bb099ef730d2c16372c4e94149648a26359feb17
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-08-24 17:07:58 +0200

    chore: Refactor video splitting logic to include silent parts

--- video_editor.py
+++ video_editor.py
@@ -5,6 +5,8 @@ from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVide
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.config import change_settings
+import numpy as np
+from pydub import AudioSegment, silence
 
 TEMP_CLIP_PATH = "./output/temp/clip.mp4"
 
@@ -49,8 +51,9 @@ def get_footage(date_str):
     ffmpeg_extract_subclip(f"./stock videos/{random_clip}", start_time, end_time, targetname=TEMP_CLIP_PATH)
     return VideoFileClip(TEMP_CLIP_PATH)
 
-def cleanup():
+def cleanup(temp_clip):
     # Remove temporary files
+    temp_clip.close()
     os.remove(TEMP_CLIP_PATH)
 
 def render(date_str):
@@ -70,7 +73,15 @@ def render(date_str):
     final_clip.duration = video_clip.duration
     final_clip.write_videofile(f"./output/final/final_{date_str}.mp4", codec='libx264', audio_codec='aac')
 
-    cleanup()
+    cleanup(video_clip)
+
+def find_silent_parts(date_str):
+    print("Finding silent sections...")
+    speech = AudioSegment.from_file(f"./output/speech/speech_{date_str}.mp3", format="mp3")
+    silent_segments = silence.detect_nonsilent(speech, min_silence_len=1000, silence_thresh=-40)
+    # Convert to array
+    silent_segments = np.array(silent_segments)
+    return silent_segments
 
 def split_video(date_str):
     # Load the video clip
@@ -80,20 +91,35 @@ def split_video(date_str):
     # Get the duration of the video
     duration = video.duration
 
+    # Find where the silent parts are, to then make sure the video is split at those points
+    silent_sections = find_silent_parts(date_str) # TODO: fix error
+    print("Done")
+
     # Calculate the duration of each part
+    MAX_PART_DURATION = 80
     nr_of_parts = 2
-    while (part_duration := duration / nr_of_parts) > 60:
+    while (part_duration := duration / nr_of_parts) > MAX_PART_DURATION:
         nr_of_parts += 1
     
-    print(f"Duration: {duration}, Part duration: {part_duration}")
-    
+    prev_ending = 0
     for i in range(nr_of_parts):
-        part = video.subclip(i * part_duration, (i + 1) * part_duration)
+        # Find the silent part that is closest to the end of this part
+        part_ending_time = prev_ending + part_duration * 1000   # Convert to milliseconds
+        print(f"Part {i+1} ending time: {part_ending_time}")
+        index = index = (np.abs(silent_sections - part_ending_time)).argmin()
+        part_ending_time = silent_sections[index]
+        part_ending_time /= 1000    # Convert back to seconds
+        print(f"Silent part found at: {part_ending_time}")
+
+        part = video.subclip(prev_ending, (i + 1) * part_duration)
         part_title_caption = subtitle_generator("Part " + str(i + 1))
         # Overlay the generator clip on the part
         part = CompositeVideoClip([part, part_title_caption.set_position(('center', 'bottom'))]) # TODO: Fix position and size
         part.duration = part_duration
         part.write_videofile(f"./output/parts/{date_str}_part{i+1}.mp4")
 
+
     # Close the video clip
     video.close()
+
+split_video("2024-08-23(1)")
