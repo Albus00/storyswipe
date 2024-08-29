d3dfdfa393ebb44e3a4c810237672d2eef327ee9
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-08-29 17:59:24 +0200

    chore: Add TikTok API integration for access token retrieval and video upload

--- tiktok_manager.py
+++ tiktok_manager.py
@@ -28,19 +28,60 @@
 
 # upload_videos()
 
-import os
-from TikTokUploader.uploader import uploadVideo
+import requests
+
+def get_access_token():
+  url = 'https://open.tiktokapis.com/v2/oauth/token/'
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cache-Control': 'no-cache'
+  }
+  data = {
+    'client_key': 'CLIENT_KEY',
+    'client_secret': 'CLIENT_SECRET',
+    'code': 'CODE',
+    'grant_type': 'authorization_code',
+    'redirect_uri': 'REDIRECT_URI'
+  }
+
+  response = requests.post(url, headers=headers, data=data)
+
+  # Check the response status code
+  if response.status_code == 200:
+    # Request was successful
+    access_token = response.json().get('access_token')
+    print("Access token:", access_token)
+    return access_token
+  else:
+    # Request failed
+    print("Request failed with status code:", response.status_code)
+    return None
 
 def upload_videos(date_str='2024-08-23(1)'):
   PATH = f"output/parts/{date_str}"
+  
+  url = 'https://open.tiktokapis.com/v2/post/publish/inbox/video/init/'
+  headers = {
+      'Authorization': 'Bearer act.example12345Example12345Example',
+      'Content-Type': 'application/json'
+  }
+  data = {
+      "source_info": {
+          "source": "FILE_UPLOAD",
+          "video_size": exampleVideoSize,
+          "chunk_size" : exampleVideoSize,
+          "total_chunk_count": 1
+      }
+  }
+  
+  response = requests.post(url, headers=headers, json=data)
+  
+  # Check the response status code
+  if response.status_code == 200:
+      # Request was successful
+      print("Request successful!")
+  else:
+      # Request failed
+      print("Request failed with status code:", response.status_code)
 
-  session_id = os.getenv("TIKTOK_SESSION_ID")
-  file = f"{PATH}_part1.mp4"
-  title = "MY SUPER TITLE"
-  tags = ["Funny", "Joke", "fyp"]
-  users = ["amazing dear"]
-
-  # Publish the video
-  uploadVideo(session_id, file, title, tags, users, url_prefix="www")
-
-upload_videos()
+get_access_token()
